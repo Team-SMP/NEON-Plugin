f1fbd33108c99fe6e4ee18f7c6cccfa0c60497cb
JunglTemple <tubikardu@gmail.com>
2024-02-19 11:03:32 +0000

    All of the basic stuffs.

---
+++ src/main/java/io/github/neonplugin/GuardCommand.java
@@ -0,0 +1,4 @@
+package io.github.neonplugin;
+
+public class GuardCommand {
+}
---
+++ src/main/java/io/github/neonplugin/NEONPlugin.java
@@ -0,0 +1,16 @@
+package io.github.neonplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NEONPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
