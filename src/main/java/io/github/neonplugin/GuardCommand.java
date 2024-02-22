4d60d4f20b586120f0470fbce569d9fe71034447
JunglTemple <tubikardu@gmail.com>
2024-02-22 18:28:16 +0000

    Did some more stuff...

--- src/main/java/io/github/neonplugin/GuardCommand.java
+++ src/main/java/io/github/neonplugin/GuardCommand.java
@@ -1,26 +1,32 @@
 package io.github.neonplugin;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
-public class GuardCommand implements CommandExecutor, TabExecutor {
+public class GuardCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender
-            return true;
+        if (sender instanceof Player player) {
             if (args.length == 1 && args[0].equals("warp")) {
                 player.teleport(new Location(Bukkit.getWorld("world"), 9825, 73, 9951));
-                final Component component = MiniMessage.miniMessage().deserialize(
-                        "You have been warped to <b><gradient:#7a33ff:#0128ab>TECH NEXUS</gradient></b>!"
-                );
+                final Component component = MiniMessage.miniMessage().deserialize("You have been warped to <b><gradient:#7a33ff:#0128ab>TECH NEXUS</gradient></b>!");
+                player.sendMessage(component);
+                return true;
+            }
+            if (args.length > 0 && args[0].equals("lockdown")) {
+                if (args[1].equals("toggle")) {
+                    return true;
+                }
+                if (args[1].equals("silence")) {
+                    return true;
+                }
+                return false;
             }
         }
         return false;
--- src/main/java/io/github/neonplugin/NEONPlugin.java
+++ src/main/java/io/github/neonplugin/NEONPlugin.java
@@ -7,11 +7,15 @@ public final class NEONPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        this.getCommand("tnguard").setExecutor(new CommandKit()); // register guard command
+        getLogger().info("Enabling the NEON Plugin...");
+        this.getCommand("tnguard").setExecutor(new GuardCommand()); // register guard command
+        getLogger().info("Command '/tnguard' registered!");
+        getLogger().info("Yay, the NEON Plugin is now enabled :)");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("The NEON Plugin is disabling. You really had to leave?");
     }
 }
